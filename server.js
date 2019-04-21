503a61deda58aeea27a0d15f6e71586ab805b633
Nrin <Nrin3141@users.noreply.github.com>
2019-04-21 05:30:58 +0200

    Add npm modules + frameguard

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 var server = app.listen(4000);
 
 var cors = require("cors");
+const frameguard = require("frameguard");
+
+app.use(
+  frameguard({
+    action: "allow-from",
+    domain: "https://ricotrebeljahr.de"
+  })
+);
 
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
