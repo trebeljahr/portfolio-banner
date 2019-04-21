46ee49844100d4c9f9bf4f5e99fa7f492c39dd33
Nrin <Nrin3141@users.noreply.github.com>
2019-04-21 04:36:28 +0200

    Enable CORS headers

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
 var server = app.listen(4000);
-app.use(express.static("public"));
+var cors = require("cors");
+
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
 console.log("Server is running!");
