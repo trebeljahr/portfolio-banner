76eb01b183669974ad9b2bfac14acff911da30d3
Nrin <Nrin3141@users.noreply.github.com>
2019-04-21 04:59:07 +0200

    Add CORS Options

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require("express");
 var app = express();
 var server = app.listen(4000);
 var cors = require("cors");
+const whitelist = ["https://ricotrebeljahr.de/coding", "http://localhost:3000"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + "/public"));
 
 console.log("Server is running!");
