cfd5d4cd4671d56cb56f016a0ef08aa7e41d317f
Nrin <Nrin3141@users.noreply.github.com>
2019-04-21 16:15:50 +0200

    .

--- Dot.js
+++ Dot.js
@@ -1,6 +1,6 @@
 class Dot {
   constructor(x, y, w) {
-    this.pos = createVector(random(width), random(height / 2));
+    this.pos = createVector(random(0, width), random(0, height / 2));
     this.target = createVector(x, y);
     this.vel = p5.Vector.random2D();
     this.acc = createVector();
