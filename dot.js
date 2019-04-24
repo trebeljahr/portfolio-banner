bc597305fd04da212fcadeabc6f7a9bb96591614
Nrin <Nrin3141@users.noreply.github.com>
2019-04-24 10:39:02 +0200

    Change arrow syntax into functions

--- dot.js
+++ dot.js
@@ -7,28 +7,33 @@ const Dot = class Dot {
     this.r = w < 600 ? (w < 400 ? 1.5 : 3) : 5;
     this.maxspeed = 10;
     this.maxforce = 1;
+    this.show = this.show.bind(this);
+    this.applyForce = this.applyForce.bind(this);
+    this.update = this.update.bind(this);
+    this.behaviors = this.behaviors.bind(this);
+    this.arrive = this.arrive.bind(this);
   }
-  show = (r, g, b) => {
+  show(r, g, b) {
     stroke(r, g, b);
     strokeWeight(this.r);
     point(this.pos.x, this.pos.y);
-  };
-  applyForce = f => {
+  }
+  applyForce(f) {
     this.acc.add(f);
-  };
-  update = () => {
+  }
+  update() {
     this.pos.add(this.vel);
     this.vel.add(this.acc);
     this.acc.mult(0);
-  };
-  behaviors = () => {
+  }
+  behaviors() {
     var arrive = this.arrive(this.target);
 
     arrive.mult(0.5);
 
     this.applyForce(arrive);
-  };
-  arrive = target => {
+  }
+  arrive(target) {
     var desired = p5.Vector.sub(target, this.pos);
     var d = desired.mag();
     var speed = this.maxspeed;
@@ -39,5 +44,5 @@ const Dot = class Dot {
     var steer = p5.Vector.sub(desired, this.vel);
     steer.limit(this.maxforce);
     return steer;
-  };
+  }
 };
